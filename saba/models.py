3b2a418f5b5289abccf6cc850d67444ae57e6d33
Bamdad <bamdadmeh.work@gmail.com>
2025-05-28 01:54:06 +0200

    railwa4

--- saba/models.py
+++ saba/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Item(models.Model):
 
     name = models.CharField(max_length=100)
+    icon = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
